88d0f768f083f7072265504c16168c47b2e236fc
Abror <135578437+eagleabror@users.noreply.github.com>
2023-09-08 21:04:16 +0500

    Revert "section"
    This reverts commit 911f8619fbefcdb6eadc203746799e0c59163438.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { useTranslation } from "react-i18next";
 import Navbar from "./components/navbar/Navbar";
-import Section from "./components/section/Section";
 
 function App() {
   const { t, i18n } = useTranslation()
   return (
     <div>
       <Navbar />
-      <Section />
     </div>
   );
 }
